b6b7e5a3788ea541c7226fcfa17921b267141e94
Ian Hunter <ijh.hunter@gmail.com>
2019-02-07 02:21:37 -0500

    add vuetify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,39 @@
 <template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>open_in_new</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-
-import VueetDialog from '@/components/VueetDialog'
+import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    VueetDialog
+    HelloWorld
   },
   data () {
     return {
-      dialogVueet: false
+      //
     }
-  },
-  methods: {
-    ...mapActions(['logout'])
   }
 }
 </script>
-
-<style>
-
-</style>
